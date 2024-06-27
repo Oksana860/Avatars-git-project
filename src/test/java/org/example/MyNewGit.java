c3ffc7d17a6e47f97d4a464d861dc120870de5a7
Oksana860 <maroksana96@gmail.com>
2024-06-26 20:29:46 -0500

    Pushing class

---
+++ src/test/java/org/example/MyNewGit.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MyNewGit {
+    public static class Testing {
+        public static void main(String[] arg) {
+            System.out.println("Hello wold");
+            System.out.println("I want to get a new job");
+            System.out.println("Life is beautiful");
+        }
+    }
+}
